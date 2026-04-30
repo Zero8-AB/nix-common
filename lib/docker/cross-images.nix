27fcfb31ecc7de9be2c81e6bea2c1cc48cfa2a92
kahre-zero8 <jonathan.kahre@zero8.io>
2026-04-30 23:59:18 +0200

    Fix inverted system ref

--- lib/docker/cross-images.nix
+++ lib/docker/cross-images.nix
@@ -6,14 +6,14 @@
 if system == "x86_64-linux"
 then {
   docker-arm64 = mkImage {
-    inherit system;
+    system = "aarch64-linux";
     pkgs = pkgs.pkgsCross.aarch64-multiplatform;
   };
 }
 else if system == "aarch64-linux"
 then {
   docker-amd64 = mkImage {
-    inherit system;
+    system = "x86_64-linux";
     pkgs = pkgs.pkgsCross.x86_64-linux;
   };
 }
