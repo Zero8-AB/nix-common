3c96c6223ab8acbcd2f9ed86ea200e45831422c7
kahre-zero8 <jonathan.kahre@zero8.io>
2026-04-30 21:55:26 +0200

    Forward system to mkImage in docker cross

--- lib/docker/cross-images.nix
+++ lib/docker/cross-images.nix
@@ -3,12 +3,18 @@
   system,
   mkImage,
 }:
-  if system == "x86_64-linux"
-  then {
-    docker-arm64 = mkImage pkgs.pkgsCross.aarch64-multiplatform;
-  }
-  else if system == "aarch64-linux"
-  then {
-    docker-amd64 = mkImage pkgs.pkgsCross.x86_64-linux;
-  }
-  else {}
+if system == "x86_64-linux"
+then {
+  docker-arm64 = mkImage {
+    inherit system;
+    pkgs = pkgs.pkgsCross.aarch64-multiplatform;
+  };
+}
+else if system == "aarch64-linux"
+then {
+  docker-amd64 = mkImage {
+    inherit system;
+    pkgs = pkgs.pkgsCross.x86_64-linux;
+  };
+}
+else {}
