8081e08094be0d9b53ee890d782593c8fcf7d6d2
kahre-zero8 <jonathan.kahre@zero8.io>
2026-06-16 21:27:12 +0200

    Add more lints to go mkLint

--- lib/go/go-lint.nix
+++ lib/go/go-lint.nix
@@ -9,15 +9,33 @@ pkgs.buildGoModule {
   pname = "${pname}-lint";
   inherit version src vendorHash subPackages;
 
-  nativeBuildInputs = [pkgs.golangci-lint];
+  nativeBuildInputs = [pkgs.golangci-lint pkgs.gotools pkgs.go-tools];
 
   checkPhase = ''
     runHook preCheck
 
     export HOME=$(mktemp -d)
 
-    golangci-lint run ./...
+    gofiles=$(find . -path ./vendor -prune -o -name '*.go' -print)
+    if [ -n "$gofiles" ]; then
+      unformatted=$(gofmt -l $gofiles)
+      if [ -n "$unformatted" ]; then
+        echo "gofmt: the following files are not formatted:" >&2
+        echo "$unformatted" >&2
+        exit 1
+      fi
+
+      imports=$(goimports -l $gofiles)
+      if [ -n "$imports" ]; then
+        echo "goimports: the following files have unorganized imports:" >&2
+        echo "$imports" >&2
+        exit 1
+      fi
+    fi
+
     go vet ./...
+    golangci-lint run ./...
+    staticcheck ./...
 
     runHook postCheck
   '';
